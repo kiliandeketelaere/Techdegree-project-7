7f6ba7122a31dadb3cb23255cfb26979065a91d3
Kilin <deketelaerekilian@live.be>
2022-04-10 13:16:30 +0700

    Finished before review

---
+++ js.js
@@ -0,0 +1,8 @@
+
+const buttonMessages = document.querySelector('.alert button')
+const alert = document.querySelector('.alert')
+
+buttonMessages.addEventListener('click', () => {
+alert.style.display = 'none'
+
+})
